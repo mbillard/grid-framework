3ca7a669179c16c8fbbe928537fb1f2367f5027d
Michel Billard <michel@mbillard.com>
2015-05-10 17:24:40 -0400

    Added a gulp task to minify the stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minify = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 gulp.task('default', ['sass'], function() { });
 
 gulp.task('sass', function () {
   gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('compile', ['sass'], function() {
+  gulp.src('./css/*.css')
+    .pipe(minify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(gulp.dest('./css'));
 });
 
